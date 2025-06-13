fa3ac4bdfe4ac663bea0c3ea7cc9426bdd7f0c28
Adembenzineb <adem.benzineb.dev@gmail.com>
2025-06-13 11:42:14 +0100

    Starting with adding div and button

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("container");
+
+
+const containerW = 800;
+const containerH = 600;
+container.style.width = `${containerW}px`;
+container.style.height = `${containerH}px`;
