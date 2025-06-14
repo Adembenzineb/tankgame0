4ed28dc66c741413b0fd0c602db44037e758028c
Adembenzineb <adem.benzineb.dev@gmail.com>
2025-06-14 16:15:00 +0100

    Create grids in the container

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+//Ajusting the size of the container
 const container = document.getElementById("container");
-
-
 const containerW = 800;
-const containerH = 600;
+const containerH = 800;
 container.style.width = `${containerW}px`;
 container.style.height = `${containerH}px`;
+
+//Creating grids
+const gridnum = 10;
+const gridSize = containerW / gridnum;
+for (let i = 0; i < gridnum**2; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid");
+    container.appendChild(div);
+}
