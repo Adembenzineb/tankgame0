31eb449ffcc07b70066bac97bdf4f1fd2b7aec2f
Adembenzineb <adem.benzineb.dev@gmail.com>
2025-06-14 18:40:04 +0100

    Fix the grid problem and finish it

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 //Ajusting the size of the container
 const container = document.getElementById("container");
-const containerW = 800;
-const containerH = 800;
-container.style.width = `${containerW}px`;
-container.style.height = `${containerH}px`;
+const containerSize = 800;
 
 //Creating grids
-const gridnum = 10;
-const gridSize = containerW / gridnum;
+const gridnum = 13;
+container.setAttribute("style" ,`grid-template-columns: repeat(${gridnum}, 1fr); grid-template-rows: repeat(${gridnum}, 1fr); width : ${containerSize}px; height: ${containerSize}px;`);
+const gridSize = containerSize / gridnum;
 for (let i = 0; i < gridnum**2; i++) {
     let div = document.createElement("div");
     div.classList.add("grid");
